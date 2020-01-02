4a2d2ef7c9305e68e2fd0345a2f5495a33473084
Gouthum Karadi <goukaradi@hotmail.com>
2020-01-01 20:47:35 -0500

    Created functions in controller.  Tested curl.  Still no create functionality.

--- src/controllers/Yoga.js
+++ src/controllers/Yoga.js
@@ -7,26 +7,26 @@ module.exports = {
   // Inside each controller method, you're defining the logic of your app
   // Each 'endpoint' will contain specific instructions...
   // ...on what data should be displayed
-  index: (req, res) => {
+  index: async (req, res, next) => {
     Yoga.find({}).then( (yoga) => {
       res.json(yoga)
     })
   },
-  create: async (req,res) => {
+  create: async (req,res, next) => {
     const asana = new Yoga({
-      id: req.body.number,
+      id: req.body.id,
       sanskrit_name: req.body.sanskrit_name,
       english_name: req.body.english_name,
-      img_url: req.body.img_url,
+      img_url: req.body.img_url
     })
     try {
       const newAsana = await asana.save()
-      req.status(201).json(newAsana)
+      return req.status(201).json(newAsana)
     }
     catch (err){
-      req.status(400).json({messags: err.message})
+      return req.status(400).json({messags: err.message})
     }
   },
-  update: (req,res) => {},
-  delete: (req,res) => {}
+  update: (req,res, next) => {},
+  delete: (req,res, next) => {}
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const app = express();
 const yogaRoutes = require("./routes/Yoga");
+const bodyParser = require('body-parser');
+
+// parse requests
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.use(express.json())
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 // .listen() - application will wait for requests from a specific port
 app.listen(8080, () => console.log('Port 8080, Ommmm.') )
